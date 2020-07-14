34937db81f5f3dc2d9ed949e38eac234452129d5
Tommaso Visconti <tommaso.visconti@adroll.com>
2020-07-14 16:31:59 +0200

    add basic example

---
+++ examples/basic/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"strings"
+	"time"
+
+	"github.com/AdRoll/baker"
+	"github.com/AdRoll/baker/filter"
+	"github.com/AdRoll/baker/input"
+	"github.com/AdRoll/baker/output"
+)
+
+var fields = map[string]baker.FieldIndex{
+	"fieldA": 0,
+	"fieldB": 1,
+	"fieldC": 2,
+}
+
+func fieldByName(key string) (baker.FieldIndex, bool) {
+	idx, ok := fields[key]
+	return idx, ok
+}
+
+func main() {
+	toml := `
+[input]
+name = "List"
+[input.config]
+    files=["./examples/data/list-clause-files-comma-sep.csv.zst"]
+[output]
+name = "Files"
+procs=1
+    [output.config]
+    PathString="/tmp/baker/output-list-clause-files-comma-sep.csv.gz"
+	`
+	c := baker.Components{
+		Inputs:      input.All,
+		Filters:     filter.All,
+		Outputs:     output.All,
+		FieldByName: fieldByName,
+	}
+	cfg, err := baker.NewConfigFromToml(strings.NewReader(toml), c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var duration time.Duration
+	err = baker.Main(cfg, duration)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
