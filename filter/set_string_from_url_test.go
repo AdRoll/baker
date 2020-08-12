086fb8fe2bd56d6c56471194637284b640705a9c
Aurélien Rainone <aurelien.rainone@gmail.com>
2020-08-12 19:01:00 +0200

    filter: add SetStringFromURL filter
    This filter is needed for the FeedParser topology, but making it
    more generic has no cost, so we end up with a filter that looks
    for some user-defined strings in a record metadata URL key, such
    as records read with the Files input. If one of the user-defined
    strings is found in the URL, this new filter will sets an
    user-defined field to that string. Records for which the URL
    doesn't contain any of the strings are discarded.

---
+++ filter/set_string_from_url_test.go
@@ -0,0 +1,74 @@
+package filter
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/AdRoll/baker"
+	"github.com/AdRoll/baker/input/inpututils"
+)
+
+func TestSetStringFromURL(t *testing.T) {
+	tests := []struct {
+		name        string
+		url         string
+		strings     []string
+		wantDiscard bool   // whether the record should have been discarded
+		wantField   string // expected field value in case the record wasn't discaded
+	}{
+		{
+			name:        "string not found in url",
+			url:         "s3://no-region",
+			strings:     []string{"us-west-2"},
+			wantDiscard: true,
+		},
+		{
+			name:        "string found",
+			url:         "s3://foo/bar/path-to-us-west-2",
+			strings:     []string{"us-west-2"},
+			wantDiscard: false,
+			wantField:   "us-west-2",
+		},
+		{
+			name:        "found, multiple strings",
+			url:         "s3://foo/bar/path-to-us-west-1",
+			strings:     []string{"us-west-2", "us-west-1", "eu-west-2"},
+			wantDiscard: false,
+			wantField:   "us-west-1",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Forge a log line having tt.url as metadata
+			u, err := url.Parse(tt.url)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			ll := &baker.LogLine{}
+			ll.Parse(nil, &baker.Metadata{inpututils.MetadataURL: u})
+
+			// Create and setup a filter with tt.strings
+			strings := make([][]byte, 0, len(tt.strings))
+			for _, s := range tt.strings {
+				strings = append(strings, []byte(s))
+			}
+
+			discarded := true
+			f := &setStringFromURL{field: 0, strings: strings}
+			f.Process(ll, func(baker.Record) { discarded = false })
+
+			if discarded != tt.wantDiscard {
+				t.Errorf("record discarded=%t, want %t", discarded, tt.wantDiscard)
+			}
+
+			if !tt.wantDiscard {
+				b := ll.Get(0)
+				if string(b) != tt.wantField {
+					t.Errorf("got field=%q, want %q", b, tt.wantField)
+				}
+			}
+		})
+	}
+}
