ea2e7d15bb3e0cce6b81c67970e1ee2d6a2753c0
Aurélien Rainone <aurelien.rainone@gmail.com>
2020-09-09 12:44:21 +0200

    examples/metrics: add custom metrics example

---
+++ examples/metrics/foobar.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"time"
+
+	"github.com/AdRoll/baker"
+	"github.com/AdRoll/baker/metrics"
+)
+
+// fooBarDesc describes how to plug fooBar as a metrics backend to Baker.
+var fooBarDesc = baker.MetricsDesc{
+	Name:   "FooBar",
+	Config: &fooBarMetricsCfg{},
+	New:    newFooBarMetrics,
+}
+
+type fooBarMetricsCfg struct {
+	Host string
+	Port int
+}
+
+type fooBarMetrics struct{}
+
+func newFooBarMetrics() (metrics.Client, error) {
+	return fooBarMetrics{}, nil
+}
+
+// Gauge sets the value of a metric of type gauge. A Gauge represents a
+// single numerical data point that can arbitrarily go up and down.
+func (fooBarMetrics) Gauge(name string, value float64) {}
+
+// GaugeWithTags sets the value of a metric of type gauge and associates
+// that value with a set of tags.
+func (fooBarMetrics) GaugeWithTags(name string, value float64, tags []string) {}
+
+// RawCount sets the value of a metric of type counter. A counter is a
+// cumulative metrics that can only increase. RawCount sets the current
+// value of the counter.
+func (fooBarMetrics) RawCount(name string, value int64) {}
+
+// RawCountWithTags sets the value of a metric or type counter and associates
+// that value with a set of tags.
+func (fooBarMetrics) RawCountWithTags(name string, value int64, tags []string) {}
+
+// DeltaCount increments the value of a metric of type counter by delta.
+// delta must be positive.
+func (fooBarMetrics) DeltaCount(name string, delta int64) {}
+
+// DeltaCountWithTags increments the value of a metric or type counter and
+// associates that value with a set of tags.
+func (fooBarMetrics) DeltaCountWithTags(name string, delta int64, tags []string) {}
+
+// Histogram adds a sample to a metric of type histogram. A histogram
+// samples observations and counts them in different 'buckets' in order
+// to track and show the statistical distribution of a set of values.
+func (fooBarMetrics) Histogram(name string, value float64) {}
+
+// HistogramWithTags adds a sample to an histogram and associates that
+// sample with a set of tags.
+func (fooBarMetrics) HistogramWithTags(name string, value float64, tags []string) {}
+
+// Duration adds a duration to a metric of type histogram. A histogram
+// samples observations and counts them in different 'buckets'. Duration
+// is basically an histogram but allows to sample values of type time.Duration.
+func (fooBarMetrics) Duration(name string, value time.Duration) {}
+
+// DurationWithTags adds a duration to an histogram and associates that
+// duration with a set of tags.
+func (fooBarMetrics) DurationWithTags(name string, value time.Duration, tags []string) {}
---
+++ examples/metrics/main.go
@@ -0,0 +1,66 @@
+// metrics example illustrates how to implement and plug a metrics interface
+// to Baker.
+package main
+
+import (
+	"log"
+	"strings"
+	"time"
+
+	"github.com/AdRoll/baker"
+	"github.com/AdRoll/baker/input"
+	"github.com/AdRoll/baker/output"
+)
+
+// Some example fields
+const (
+	Timestamp baker.FieldIndex = 0
+	Source    baker.FieldIndex = 1
+	Target    baker.FieldIndex = 2
+)
+
+var fields = map[string]baker.FieldIndex{
+	"timestamp": Timestamp,
+	"source":    Source,
+	"target":    Target,
+}
+
+func fieldByName(key string) (baker.FieldIndex, bool) {
+	idx, ok := fields[key]
+	return idx, ok
+}
+
+func main() {
+	toml := `
+[metrics]
+name="Foobar"
+
+[input]
+name = "List"
+
+	[input.config]
+	files=["testdata/input.csv.zst"]
+
+[output]
+name = "FileWriter"
+procs=1
+
+	[output.config]
+	PathString="./_out/output.csv.gz"
+`
+	c := baker.Components{
+		Inputs:      input.All,
+		Outputs:     output.All,
+		FieldByName: fieldByName,
+		Metrics:     []baker.MetricsDesc{fooBarDesc},
+	}
+	cfg, err := baker.NewConfigFromToml(strings.NewReader(toml), c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var duration time.Duration
+	err = baker.Main(cfg, duration)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
