60bcbfa99bbc4b075e1419bacb44ed2ca3ccd930
Tommaso Visconti <tommaso.visconti@adroll.com>
2020-09-21 10:52:27 +0200

    Add the Concatenate filter (#33)
    * Add the Concatenate filter
    
    * use a 1-byte separator
    
    * change how it checks the separator value
    
    * Update filter/concatenate.go
    
    Co-authored-by: Aurélien Rainone <476650+arl@users.noreply.github.com>
    
    * use separator as bytes slice
    
    * clarify separator check error
    
    Co-authored-by: Aurélien Rainone <476650+arl@users.noreply.github.com>

---
+++ filter/concatenate.go
@@ -0,0 +1,92 @@
+package filter
+
+import (
+	"errors"
+	"fmt"
+	"sync/atomic"
+	"unicode"
+
+	"github.com/AdRoll/baker"
+)
+
+var ConcatenateDesc = baker.FilterDesc{
+	Name:   "Concatenate",
+	New:    NewConcatenate,
+	Config: &ConcatenateConfig{},
+	Help:   `Concatenate up to 10 fields' values to a single field`,
+}
+
+type ConcatenateConfig struct {
+	Fields    []string `help:"The field names to concatenate, in order"`
+	Target    string   `help:"The field name to save the concatenated value to"`
+	Separator string   `help:"Separator to concatenate the values. Must either be empty or a single ASCII, non-nil char"`
+}
+
+type Concatenate struct {
+	numProcessedLines int64
+	numFilteredLines  int64
+	fields            []baker.FieldIndex
+	target            baker.FieldIndex
+	separator         []byte
+}
+
+func NewConcatenate(cfg baker.FilterParams) (baker.Filter, error) {
+	if cfg.DecodedConfig == nil {
+		cfg.DecodedConfig = &ConcatenateConfig{}
+	}
+	dcfg := cfg.DecodedConfig.(*ConcatenateConfig)
+
+	fields := []baker.FieldIndex{}
+	for _, fieldName := range dcfg.Fields {
+		i, ok := cfg.FieldByName(fieldName)
+		if !ok {
+			return nil, fmt.Errorf("Can't resolve field %s", fieldName)
+		}
+		fields = append(fields, i)
+	}
+
+	target, ok := cfg.FieldByName(dcfg.Target)
+	if !ok {
+		return nil, fmt.Errorf("Can't resolve target field %s", dcfg.Target)
+	}
+
+	var separator []byte
+	if dcfg.Separator != "" {
+		if len(dcfg.Separator) != 1 || dcfg.Separator[0] > unicode.MaxASCII {
+			return nil, errors.New("Separator must either be empty or a single ASCII, non-nil char")
+		}
+		separator = append(separator, dcfg.Separator[0])
+	}
+
+	f := &Concatenate{
+		fields:    fields,
+		target:    target,
+		separator: separator,
+	}
+
+	return f, nil
+}
+
+func (c *Concatenate) Stats() baker.FilterStats {
+	return baker.FilterStats{
+		NumProcessedLines: atomic.LoadInt64(&c.numProcessedLines),
+		NumFilteredLines:  atomic.LoadInt64(&c.numFilteredLines),
+	}
+}
+
+func (c *Concatenate) Process(l baker.Record, next func(baker.Record)) {
+	atomic.AddInt64(&c.numProcessedLines, 1)
+
+	key := make([]byte, 0, 512)
+	flen := len(c.fields) - 1
+	for i, f := range c.fields {
+		v := l.Get(f)
+		if i < flen {
+			v = append(v, c.separator...)
+		}
+		key = append(key, v...)
+	}
+
+	l.Set(c.target, key)
+	next(l)
+}
---
+++ filter/concatenate_test.go
@@ -0,0 +1,114 @@
+package filter
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/AdRoll/baker"
+)
+
+func TestConcatenateFilter(t *testing.T) {
+	tests := []struct {
+		name      string
+		fields    []string
+		record    string
+		separator string
+		want      string
+		wantErr   bool
+	}{
+		{
+			name:   "without separator",
+			record: "v0,v1,v2,\n",
+			fields: []string{"src2", "src0", "src1"},
+			want:   "v2v0v1",
+		},
+		{
+			name:      "with separator",
+			record:    "v0,v1,v2,\n",
+			fields:    []string{"src2", "src0", "src1"},
+			separator: "~",
+			want:      "v2~v0~v1",
+		},
+		{
+			name:      "overwrites target",
+			record:    "v0,v1,v2,trgt\n",
+			fields:    []string{"src2", "src0", "src1"},
+			separator: "~",
+			want:      "v2~v0~v1",
+		},
+		{
+			name:      "byte separator",
+			record:    "v0,v1,v2,trgt\n",
+			fields:    []string{"src2", "src0", "src1"},
+			separator: "\u007c",
+			want:      "v2|v0|v1",
+		},
+		{
+			name:      "explicit empty string separator",
+			record:    "v0,v1,v2,trgt\n",
+			fields:    []string{"src2", "src0", "src1"},
+			separator: "",
+			want:      "v2v0v1",
+		},
+		{
+			name:      "wrong separator",
+			record:    "v0,v1,v2,trgt\n",
+			fields:    []string{"src2", "src0", "src1"},
+			separator: "è",
+			wantErr:   true,
+		},
+	}
+
+	fieldByName := func(name string) (baker.FieldIndex, bool) {
+		switch name {
+		case "src0":
+			return 0, true
+		case "src1":
+			return 1, true
+		case "src2":
+			return 2, true
+		case "target":
+			return 3, true
+		}
+		return 0, false
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			params := baker.FilterParams{
+				ComponentParams: baker.ComponentParams{
+					FieldByName: fieldByName,
+					DecodedConfig: &ConcatenateConfig{
+						Fields:    tt.fields,
+						Target:    "target",
+						Separator: tt.separator,
+					},
+				},
+			}
+
+			ll := baker.LogLine{FieldSeparator: ','}
+			err := ll.Parse([]byte(tt.record), nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			f, err := NewConcatenate(params)
+			if tt.wantErr {
+				if err == nil {
+					t.Fatal("Expected conf err")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			f.Process(&ll, func(baker.Record) {})
+
+			if !bytes.Equal(ll.Get(3), []byte(tt.want)) {
+				t.Errorf("got: %q, want: %q", ll.Get(3), tt.want)
+			}
+		})
+	}
+}
