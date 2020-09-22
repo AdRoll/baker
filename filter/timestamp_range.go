7c3203fe55a7126d6b8ef5140b0779cb6bc26a23
Aurélien Rainone <476650+arl@users.noreply.github.com>
2020-09-22 14:17:42 +0200

    Open source timestamp range filter (#46)
    * filter: add timestamp_range*.go as-is
    
    * filter: add TimestampRange doc
    
    * filter: add TimestampRange filter
    
    * Update CHANGELOG.md
    
    * Update filter/timestamp_range.go
    
    Co-authored-by: Tommaso Visconti <tommaso.visconti@adroll.com>
    
    Co-authored-by: Tommaso Visconti <tommaso.visconti@adroll.com>

---
+++ filter/timestamp_range.go
@@ -0,0 +1,97 @@
+package filter
+
+import (
+	"fmt"
+	"strconv"
+	"sync/atomic"
+	"time"
+
+	"github.com/AdRoll/baker"
+)
+
+// TimestampRangeDesc describes the NotNull filter.
+var TimestampRangeDesc = baker.FilterDesc{
+	Name:   "TimestampRange",
+	New:    NewTimestampRange,
+	Config: &TimestampRangeConfig{},
+	Help:   "Discard records if the value of a field containing a timestamp is out of the given time range (i.e StartDateTime <= value < EndDateTime)",
+}
+
+// TimestampRangeConfig holds configuration paramters for the NotNull filter.
+type TimestampRangeConfig struct {
+	StartDatetime string `help:"Lower bound of the accepted time interval (inclusive, UTC) format:'2006-01-31 15:04:05'" default:"no bound" required:"true"`
+	EndDatetime   string `help:"Upper bound of the accepted time interval (exclusive, UTC) format:'2006-01-31 15:04:05'" default:"no bound" required:"true"`
+	Field         string `help:"Name of the field containing the Unix EPOCH timestamp" required:"true"`
+}
+
+// TimestampRange is a baker filter that discards records depending on the
+// value of a field representing a Unix timestamp.
+type TimestampRange struct {
+	numProcessedLines int64
+	numFilteredLines  int64
+	startDate         int64
+	endDate           int64
+
+	fidx baker.FieldIndex
+}
+
+// NewTimestampRange creates and configures a TimestampRange filter.
+func NewTimestampRange(cfg baker.FilterParams) (baker.Filter, error) {
+	if cfg.DecodedConfig == nil {
+		cfg.DecodedConfig = &TimestampRangeConfig{}
+	}
+	dcfg := cfg.DecodedConfig.(*TimestampRangeConfig)
+
+	fidx, ok := cfg.FieldByName(dcfg.Field)
+	if !ok {
+		return nil, fmt.Errorf("unknown field %q", dcfg.Field)
+	}
+
+	const timeLayout = "2006-01-02 15:04:05"
+
+	s, err := time.Parse(timeLayout, dcfg.StartDatetime)
+	if err != nil {
+		return nil, fmt.Errorf("StartDateTime is invalid: %s", err)
+	}
+
+	e, err := time.Parse(timeLayout, dcfg.EndDatetime)
+	if err != nil {
+		return nil, fmt.Errorf("EndDatetime is invalid: %s", err)
+	}
+
+	f := &TimestampRange{
+		startDate: s.Unix(),
+		endDate:   e.Unix(),
+		fidx:      fidx,
+	}
+
+	return f, nil
+}
+
+// Stats implements baker.Filter.
+func (f *TimestampRange) Stats() baker.FilterStats {
+	return baker.FilterStats{
+		NumProcessedLines: atomic.LoadInt64(&f.numProcessedLines),
+		NumFilteredLines:  atomic.LoadInt64(&f.numFilteredLines),
+	}
+}
+
+// Process implements baker.Filter.
+func (f *TimestampRange) Process(l baker.Record, next func(baker.Record)) {
+	atomic.AddInt64(&f.numProcessedLines, 1)
+
+	// Convert the record timestamp to unix time (int64)
+	ts, err := strconv.ParseInt(string(l.Get(f.fidx)), 10, 64)
+	if err != nil {
+		atomic.AddInt64(&f.numFilteredLines, 1)
+		return
+	}
+
+	// Discard records having an out-of-bounds timestamp.
+	if ts < f.startDate || ts >= f.endDate {
+		atomic.AddInt64(&f.numFilteredLines, 1)
+		return
+	}
+
+	next(l)
+}
