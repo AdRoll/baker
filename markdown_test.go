7f8171d8041f425b7f8d6a43eb33c53378c861fe
Aurélien Rainone <aurelien.rainone@gmail.com>
2020-11-09 14:50:06 +0100

    Add skeleton code+test for markdown help

---
+++ markdown_test.go
@@ -0,0 +1,33 @@
+package baker
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGenerateMarkdownHelp(t *testing.T) {
+	tests := []struct {
+		name    string
+		desc    interface{}
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "nil",
+			desc:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := &bytes.Buffer{}
+			if err := GenerateMarkdownHelp(w, tt.desc); (err != nil) != tt.wantErr {
+				t.Errorf("GenerateMarkdownHelp() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotW := w.String(); gotW != tt.want {
+				t.Errorf("GenerateMarkdownHelp() = %v, want %v", gotW, tt.want)
+			}
+		})
+	}
+}
