ed97aea9419e32e84a59ee1b88c51023f599a051
Tommaso Visconti <tommaso.visconti@adroll.com>
2020-11-17 10:20:54 +0100

    use the new [fields] config

--- tutorials/basic/main.go
+++ tutorials/basic/main.go
@@ -12,43 +12,12 @@ import (
 
 func main() {
 	components := baker.Components{
-		Inputs:      input.All,
-		Filters:     filter.All,
-		Outputs:     output.All,
-		Uploads:     upload.All,
-		FieldByName: fieldByName,
-		FieldName:   fieldName,
+		Inputs:  input.All,
+		Filters: filter.All,
+		Outputs: output.All,
+		Uploads: upload.All,
 	}
 	if err := baker.MainCLI(components); err != nil {
 		log.Fatal(err)
 	}
-}
-
-var fields = []string{
-	"review_num",
-	"brand",
-	"variety",
-	"style",
-	"country",
-	"stars",
-	"top_ten",
-}
-
-var fieldsByName = map[string]baker.FieldIndex{
-	"review_num": 0,
-	"brand":      1,
-	"variety":    2,
-	"style":      3,
-	"country":    4,
-	"stars":      5,
-	"top_ten":    6,
-}
-
-func fieldByName(name string) (idx baker.FieldIndex, ok bool) {
-	idx, ok = fieldsByName[name]
-	return idx, ok
-}
-
-func fieldName(idx baker.FieldIndex) string {
-	return fields[idx]
 }
