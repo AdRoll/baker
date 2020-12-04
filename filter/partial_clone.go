a200eda95af349ed27e6d842d9bb9b5af75a01d0
Tommaso Visconti <tommaso.visconti@adroll.com>
2020-12-04 18:19:07 +0100

    only clone, without matching

--- filter/partial_clone.go
+++ filter/partial_clone.go
@@ -1,55 +1,29 @@
 package filter
 
 import (
-	"bytes"
 	"fmt"
 	"sync/atomic"
 
 	"github.com/AdRoll/baker"
 )
 
-const subRecordHelp = `
-If the record matches the expected values, the filter copies a list of fields to a new record and process this new record, discarding the original one.
-
-"Matches" configuration is a list of list of strings in the form ["fieldName", "value1", "value2", ...].
-The first element identifies the record field to check against the other elements of the list, using the OR condition (if any of the values matches the field, the condition is passed).
-Multiple entries to "Matches" are evaluated as AND, which means that all conditions must pass to match the record.
-
-Example:
-
-Matches = [["timestamp", "value1", "value2"], ["source", "host1"], ["target", "host2", "host3"]]
-
-Is equivalent to:
-
-((timestamp == "value1" OR timestamp == "value2") AND (source == "host1") AND (target == "host2" OR target == "host3"))
-
-If the provided conditions don't match the record, then the original record is discarded when DiscardNotMatching is true or is sent down the filter chain unchanged otherwise.
-
-The "Fields" configuration lists the fields to copy from the original record to the new one, in case the record matches the conditions.
-The new record will only contain the values of these fields, all other fields will be empty.
-`
-
 var PartialCloneDesc = baker.FilterDesc{
 	Name:   "PartialClone",
 	New:    NewPartialClone,
 	Config: &PartialCloneConfig{},
-	Help:   subRecordHelp,
+	Help:   "Copy a list of fields to a new record and process this new record, discarding the original one",
 }
 
 type PartialCloneConfig struct {
-	Matches            [][]string `help:"Conditions used to identify records to apply the filter to. See help for details. Missing configuration matches all records" required:"false"`
-	DiscardNotMatching bool       `help:"Discard or maintain the records not matching the conditions" required:"false" default:"false"`
-	Fields             []string   `help:"Fields that must be copied to the new line" required:"true"`
+	Fields []string `help:"Fields that must be copied to the new line" required:"true"`
 }
 
 type PartialClone struct {
 	numProcessedLines int64
 	numFilteredLines  int64
 
-	matches            map[baker.FieldIndex][][]byte
-	discardNotMatching bool
-	fieldIdx           []baker.FieldIndex
-	createRecord       func() baker.Record
+	fieldIdx     []baker.FieldIndex
+	createRecord func() baker.Record
 }
 
 func NewPartialClone(cfg baker.FilterParams) (baker.Filter, error) {
@@ -57,6 +31,10 @@ func NewPartialClone(cfg baker.FilterParams) (baker.Filter, error) {
 		cfg.DecodedConfig = &PartialCloneConfig{}
 	}
 	dcfg := cfg.DecodedConfig.(*PartialCloneConfig)
+
+	if len(dcfg.Fields) == 0 {
+		return nil, fmt.Errorf("PartialClone: add at least one field")
+	}
 
 	fieldIdx := []baker.FieldIndex{}
 	for _, f := range dcfg.Fields {
@@ -66,23 +44,9 @@ func NewPartialClone(cfg baker.FilterParams) (baker.Filter, error) {
 		}
 		fieldIdx = append(fieldIdx, idx)
 	}
-	matches := make(map[baker.FieldIndex][][]byte)
-
-	for _, f := range dcfg.Matches {
-		idx, ok := cfg.FieldByName(f[0])
-		if !ok {
-			return nil, fmt.Errorf("Can't resolve field name %ss", f[0])
-		}
-		for i := 1; i < len(f); i++ {
-			matches[idx] = append(matches[idx], []byte(f[i]))
-		}
-
-	}
 	ut := &PartialClone{
-		fieldIdx:           fieldIdx,
-		discardNotMatching: dcfg.DiscardNotMatching,
-		matches:            matches,
-		createRecord:       cfg.CreateRecord,
+		fieldIdx:     fieldIdx,
+		createRecord: cfg.CreateRecord,
 	}
 	return ut, nil
 }
@@ -97,35 +61,9 @@ func (s *PartialClone) Stats() baker.FilterStats {
 func (s *PartialClone) Process(r baker.Record, next func(baker.Record)) {
 	atomic.AddInt64(&s.numProcessedLines, 1)
 
-	if !s.recordMatch(r) {
-		if s.discardNotMatching {
-			atomic.AddInt64(&s.numFilteredLines, 1)
-			return
-		}
-		next(r)
-		return
-	}
-
 	l2 := s.createRecord()
 	for _, idx := range s.fieldIdx {
 		l2.Set(idx, r.Get(idx))
 	}
 	next(l2)
-}
-
-func (s *PartialClone) recordMatch(r baker.Record) bool {
-	for k, values := range s.matches {
-		fvalue := r.Get(k)
-		m := false
-		for _, v := range values {
-			if bytes.Equal(v, fvalue) {
-				m = true
-				break
-			}
-		}
-		if m == false {
-			return false
-		}
-	}
-	return true
 }
--- filter/partial_clone_test.go
+++ filter/partial_clone_test.go
@@ -1,7 +1,6 @@
 package filter
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/AdRoll/baker"
@@ -22,36 +21,21 @@ func TestNewPartialClone(t *testing.T) {
 	}
 
 	tests := []struct {
-		name               string
-		discardNotMatching bool
-		confMatches        [][]string
-		confFields         []string
-		wantFieldIdx       []baker.FieldIndex
-		wantMatches        map[baker.FieldIndex][][]byte
+		name         string
+		confFields   []string
+		wantFieldIdx []baker.FieldIndex
+		wantErr      bool
 	}{
 		{
-			name:        "base conf",
-			confMatches: [][]string{[]string{"field0", "a"}, []string{"field1", "b"}, []string{"field2", "c"}},
-			wantMatches: map[baker.FieldIndex][][]byte{
-				0: [][]byte{[]byte("a")},
-				1: [][]byte{[]byte("b")},
-				2: [][]byte{[]byte("c")},
-			},
-			confFields:         []string{"field0", "field2"},
-			wantFieldIdx:       []baker.FieldIndex{0, 2},
-			discardNotMatching: true,
+			name:         "base conf",
+			confFields:   []string{"field0", "field2"},
+			wantFieldIdx: []baker.FieldIndex{0, 2},
 		},
 		{
-			name:        "complex conf",
-			confMatches: [][]string{[]string{"field0", "a", "d", "efg"}, []string{"field1", "b", "1we%rty"}, []string{"field2", "c", "e"}},
-			wantMatches: map[baker.FieldIndex][][]byte{
-				0: [][]byte{[]byte("a"), []byte("d"), []byte("efg")},
-				1: [][]byte{[]byte("b"), []byte("1we%rty")},
-				2: [][]byte{[]byte("c"), []byte("e")},
-			},
-			confFields:         []string{"field1", "field0", "field2"},
-			wantFieldIdx:       []baker.FieldIndex{1, 0, 2},
-			discardNotMatching: false,
+			name:         "empty conf",
+			confFields:   []string{},
+			wantFieldIdx: []baker.FieldIndex{},
+			wantErr:      true,
 		},
 	}
 
@@ -61,16 +45,18 @@ func TestNewPartialClone(t *testing.T) {
 				ComponentParams: baker.ComponentParams{
 					FieldByName: fieldByName,
 					DecodedConfig: &PartialCloneConfig{
-						Matches:            tt.confMatches,
-						Fields:             tt.confFields,
-						DiscardNotMatching: tt.discardNotMatching,
+						Fields: tt.confFields,
 					},
 				},
 			}
 
 			filter, err := NewPartialClone(params)
-			if err != nil {
-				t.Fatalf("init error: %v", err)
+			if (err == nil) == tt.wantErr {
+				t.Fatalf("err: %v, wantErr: %t", err, tt.wantErr)
+			}
+
+			if tt.wantErr {
+				return
 			}
 
 			if len(filter.(*PartialClone).fieldIdx) != len(tt.confFields) {
@@ -87,24 +73,6 @@ func TestNewPartialClone(t *testing.T) {
 				}
 				if !got {
 					t.Errorf("can't find field %d in fieldIdx", idx)
-				}
-			}
-
-			if filter.(*PartialClone).discardNotMatching != tt.discardNotMatching {
-				t.Errorf("discardNotMatching, want: %t got: %t", tt.discardNotMatching, filter.(*PartialClone).discardNotMatching)
-			}
-
-			for i := range tt.confMatches {
-				field := tt.confMatches[i][0]
-				idx, _ := fieldByName(field)
-
-				m, ok := filter.(*PartialClone).matches[idx]
-				if !ok {
-					t.Errorf("can't find key %s in matches", field)
-				}
-
-				if !reflect.DeepEqual(tt.wantMatches[idx], m) {
-					t.Errorf("wrong map for field %s field, got: %v, want: %v", field, m, tt.wantMatches[idx])
 				}
 			}
 		})
@@ -126,31 +94,14 @@ func TestPartialClone_Process(t *testing.T) {
 	}
 
 	tests := []struct {
-		name               string
-		record             string
-		discardNotMatching bool
-		wantNext           bool
-		wantRecord         string
+		name       string
+		record     string
+		wantRecord string
 	}{
 		{
-			name:               "doesn't match, calls next unchanged",
-			record:             "notmatch,foo,bar",
-			discardNotMatching: false,
-			wantNext:           true,
-			wantRecord:         "notmatch,foo,bar",
-		},
-		{
-			name:               "doesn't match, doesn't calls next",
-			record:             "notmatch,foo,bar",
-			discardNotMatching: true,
-			wantNext:           false,
-		},
-		{
-			name:               "matches",
-			record:             "a,foo,bar",
-			discardNotMatching: true,
-			wantNext:           true,
-			wantRecord:         "a,,bar,,",
+			name:       "base",
+			record:     "a,foo,bar",
+			wantRecord: "a,,bar,,",
 		},
 	}
 
@@ -159,8 +110,7 @@ func TestPartialClone_Process(t *testing.T) {
 			CreateRecord: func() baker.Record { return &baker.LogLine{FieldSeparator: ','} },
 			FieldByName:  fieldByName,
 			DecodedConfig: &PartialCloneConfig{
-				Matches: [][]string{[]string{"field0", "a", "b"}},
-				Fields:  []string{"field0", "field2"},
+				Fields: []string{"field0", "field2"},
 			},
 		},
 	}
@@ -171,110 +121,19 @@ func TestPartialClone_Process(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			filter.(*PartialClone).discardNotMatching = tt.discardNotMatching
-
-			var called bool
 			var newRecord string
-			next := func(r baker.Record) {
-				called = true
+			nextFn := func(r baker.Record) {
 				newRecord = string(r.(*baker.LogLine).ToText(nil))
 			}
 
 			record := &baker.LogLine{FieldSeparator: ','}
 			record.Parse([]byte(tt.record), nil)
 
-			filter.Process(record, next)
-			if called != tt.wantNext {
-				t.Errorf("next, want: %t, got: %t", tt.wantNext, called)
-			}
-
-			if !tt.wantNext {
-				return
-			}
+			filter.Process(record, nextFn)
 
 			if newRecord != tt.wantRecord {
 				t.Errorf("record mismatch, want: %s got: %s", tt.wantRecord, newRecord)
 			}
 		})
-	}
-}
-
-func TestPartialClone_recordMatch(t *testing.T) {
-	fieldByName := func(fname string) (baker.FieldIndex, bool) {
-		switch fname {
-		case "field0":
-			return 0, true
-		case "field1":
-			return 1, true
-		case "field2":
-			return 2, true
-		default:
-			return -1, false
-		}
-	}
-
-	tests := []struct {
-		name    string
-		record  string
-		matches [][]string
-		want    bool
-	}{
-		{
-			name:    "all AND, matches",
-			matches: [][]string{[]string{"field0", "a"}, []string{"field1", "b"}, []string{"field2", "c"}},
-			record:  "a,b,c",
-			want:    true,
-		},
-		{
-			name:    "all AND, does not match",
-			matches: [][]string{[]string{"field0", "a"}, []string{"field1", "b"}, []string{"field2", "c"}},
-			record:  "a,b,d",
-			want:    false,
-		},
-		{
-			name:    "mixed, matches",
-			matches: [][]string{[]string{"field0", "a", "d"}, []string{"field1", "b"}, []string{"field2", "c"}},
-			record:  "d,b,c",
-			want:    true,
-		},
-		{
-			name:    "mixed, does not match",
-			matches: [][]string{[]string{"field0", "a", "d"}, []string{"field1", "b", "e"}, []string{"field2", "c"}},
-			record:  "d,f,c",
-			want:    false,
-		},
-		{
-			name:    "empty, matches",
-			matches: [][]string{},
-			record:  "a,b,c",
-			want:    true,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-
-			params := baker.FilterParams{
-				ComponentParams: baker.ComponentParams{
-					FieldByName: fieldByName,
-					DecodedConfig: &PartialCloneConfig{
-						Matches: tt.matches,
-					},
-				},
-			}
-
-			filter, err := NewPartialClone(params)
-			if err != nil {
-				t.Fatalf("init error: %v", err)
-			}
-
-			record := &baker.LogLine{FieldSeparator: ','}
-			record.Parse([]byte(tt.record), nil)
-
-			if filter.(*PartialClone).recordMatch(record) != tt.want {
-				t.Errorf("\"%s\" match error", tt.name)
-			}
-		})
-
 	}
 }
