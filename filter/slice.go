d42129d6ae1fa4406830b6dd03226fa313ae2baf
Aurélien Rainone <aurelien.rainone@gmail.com>
2021-04-06 11:08:57 +0200

    filter: Slice: address review comments

--- filter/slice.go
+++ filter/slice.go
@@ -10,14 +10,17 @@ var SliceDesc = baker.FilterDesc{
 	Name:   "Slice",
 	New:    NewSlice,
 	Config: &SliceConfig{},
-	Help:   "Slices the source field value using start/end indexes and copies the value to the destination field. If the start index is greater than the field value length, set an empty string to destination",
+	Help: `Slices the source field value using start/end indexes and copies the value to the destination field.
+If the start index is greater than the field length, Slice sets the destination to an empty string.
+If the end index is greater than the field length, Slice considers the end index to be equal to the field length.
+Note: Indexes are 0-based and are intended as number of bytes, thus not taking into account any encoding the values may have.`,
 }
 
 type SliceConfig struct {
 	Src      string `help:"The source field to slice" required:"true"`
 	Dst      string `help:"The destination field to save the sliced value to" required:"true"`
-	StartIdx int    `help:"The index representing the start of the slicing" default:"0"`
-	EndIdx   int    `help:"The index representing the end of the slicing. Defaults to the end of the field"`
+	StartIdx int    `help:"The index representing where the slicing starts" default:"0"`
+	EndIdx   int    `help:"The index representing where the slicind ends. Defaults to the last byte"`
 }
 
 // Slice filter slices the source field value to the given bytes length and saves the value to the destination field
@@ -34,12 +37,12 @@ func NewSlice(cfg baker.FilterParams) (baker.Filter, error) {
 
 	src, ok := cfg.FieldByName(dcfg.Src)
 	if !ok {
-		return nil, fmt.Errorf("cannot find source field \"%s\"", dcfg.Src)
+		return nil, fmt.Errorf("cannot find source field %q", dcfg.Src)
 	}
 
 	dst, ok := cfg.FieldByName(dcfg.Dst)
 	if !ok {
-		return nil, fmt.Errorf("cannot find destination field \"%s\"", dcfg.Dst)
+		return nil, fmt.Errorf("cannot find destination field %q", dcfg.Dst)
 	}
 
 	if dcfg.EndIdx <= dcfg.StartIdx && dcfg.EndIdx > 0 {
