5a5789b21e56639b10ddf44826d6ced4e04a9178
Aurélien Rainone <aurelien.rainone@gmail.com>
2021-09-02 13:19:06 +0200

    input: sqs: predeclare s3::ObjectCreated format

--- input/sqs_test.go
+++ input/sqs_test.go
@@ -21,7 +21,8 @@ func TestSQSParseMessage(t *testing.T) {
 		},
 		{
 			format: "sns",
-			message: `{
+			message: `
+			{
 				"Type" : "Notification",
 				"Message" : "s3://another-bucket/path/to/file",
 				"Timestamp" : "2023-05-22T23:21:09.550Z"
@@ -31,13 +32,102 @@ func TestSQSParseMessage(t *testing.T) {
 		{
 			format:     "json",
 			expression: "Foo.Bar",
-			message: `{
+			message: `
+			{
 				"Type": "Notification",
 				"Foo": {
 				  "Bar": "s3://another-bucket/path/to/file"
 				}
-			  }`,
+			}`,
 			wantPath: "s3://another-bucket/path/to/file",
+		},
+		{
+			format:     "s3::ObjectCreated",
+			expression: "Records[*].join('/',['s3:/', s3.bucket.name, s3.object.key]) | [0]",
+			message: `
+			{
+				"Records": [
+					{
+						"eventVersion": "2.1",
+						"eventSource": "aws:s3",
+						"awsRegion": "us-west-2",
+						"eventTime": "2021-08-29T11:52:17.371Z",
+						"eventName": "ObjectCreated:Put",
+						"userIdentity": {
+							"principalId": "REDACTED"
+						},
+						"requestParameters": {
+							"sourceIPAddress": "172.18.206.6"
+						},
+						"responseElements": {
+							"x-amz-request-id": "REDACTED",
+							"x-amz-id-2": "REDACTEDREDACTEDREDACTEDREDACTEDREDACTED"
+						},
+						"s3": {
+							"s3SchemaVersion": "1.0",
+							"configurationId": "tf-s3-topic-20210825REDACTED",
+							"bucket": {
+								"name": "mybucket",
+								"ownerIdentity": {
+									"principalId": "A3SX25GZ0Y2AT2"
+								},
+								"arn": "arn:aws:s3:::mybucket"
+							},
+							"object": {
+								"key": "path/to/a/csv/file/in/a/bucket/file.csv.log.zst",
+								"size": 88190,
+								"eTag": "9103b07ce4308641b8b7dd6491155eae",
+								"sequencer": "00612B74F551DAD52A"
+							}
+						}
+					}
+				]
+			}`,
+			wantPath: "s3://mybucket/path/to/a/csv/file/in/a/bucket/file.csv.log.zst",
+		},
+		{
+			format:     "json",
+			expression: "Records[*].join('/',['s3:/', s3.bucket.name, s3.object.key]) | [0]",
+			message: `
+			{
+				"Records": [
+					{
+						"eventVersion": "2.1",
+						"eventSource": "aws:s3",
+						"awsRegion": "us-west-2",
+						"eventTime": "2021-08-29T11:52:17.371Z",
+						"eventName": "ObjectCreated:Put",
+						"userIdentity": {
+							"principalId": "REDACTED"
+						},
+						"requestParameters": {
+							"sourceIPAddress": "172.18.206.6"
+						},
+						"responseElements": {
+							"x-amz-request-id": "REDACTED",
+							"x-amz-id-2": "REDACTEDREDACTEDREDACTEDREDACTEDREDACTED"
+						},
+						"s3": {
+							"s3SchemaVersion": "1.0",
+							"configurationId": "tf-s3-topic-20210825REDACTED",
+							"bucket": {
+								"name": "mybucket",
+								"ownerIdentity": {
+									"principalId": "A3SX25GZ0Y2AT2"
+								},
+								"arn": "arn:aws:s3:::mybucket"
+							},
+							"object": {
+								"key": "path/to/a/csv/file/in/a/bucket/file.csv.log.zst",
+								"size": 88190,
+								"eTag": "9103b07ce4308641b8b7dd6491155eae",
+								"sequencer": "00612B74F551DAD52A"
+							}
+						}
+					}
+				]
+			}`,
+			wantPath: "s3://mybucket/path/to/a/csv/file/in/a/bucket/file.csv.log.zst",
 		},
 	}
 	for _, tt := range tests {
