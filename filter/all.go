3fc9f864bf616c7255b6edb7318c3ca7fd5e71e9
Aurélien Rainone <aurelien.rainone@gmail.com>
2022-06-20 16:31:12 +0200

    filter: remove all filters [TMP]

--- filter/all.go
+++ filter/all.go
@@ -6,29 +6,4 @@ import (
 )
 
 // All is the list of all baker filters.
-var All = []baker.FilterDesc{
-	ClauseFilterDesc,
-	ClearFieldsDesc,
-	ConcatenateDesc,
-	CountAndTagDesc,
-	CryptDesc,
-	DedupDesc,
-	ExpandJSONDesc,
-	ExpandListDesc,
-	ExternalMatchDesc,
-	FormatTimeDesc,
-	HashDesc,
-	MetadataLastModifiedDesc,
-	MetadataUrlDesc,
-	NotNullDesc,
-	PartialCloneDesc,
-	RegexMatchDesc,
-	ReplaceFieldsDesc,
-	SetStringFromURLDesc,
-	SliceDesc,
-	StringMatchDesc,
-	TimestampDesc,
-	TimestampRangeDesc,
-	URLEscapeDesc,
-	URLParamDesc,
-}
+var All = []baker.FilterDesc{}
